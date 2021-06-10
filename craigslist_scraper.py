6e4905aa5df8bda98fb9976b50fe950ed298a284
lmcox <49920715+lmcox@users.noreply.github.com>
2021-06-10 17:37:13 -0500

    Create craigslist_scraper.py

---
+++ craigslist_scraper.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+@author: lmcox
+"""
+#%%
+#import get to call a get request on the site
+import requests
+from bs4 import BeautifulSoup
+from datetime import date
+import time
+import os
+
+
+#%%
+def get_html_from_pages(number_of_pages, filepath=None):
+    current_page = 360
+    current_date = str(date.today())
+    timestamp = str(time.time())
+    save_file_text = 'The ' + str(number_of_pages) + 'pages of posts in this directory were downloaded on ' + current_date + ' at ' + timestamp
+    if filepath != None:
+        savefile = os.path.join(filepath, '__date_saved.txt')
+    else: savefile = '__date_saved.txt'
+    with open(savefile, 'w') as f:
+        f.write(save_file_text)
+    while (current_page/120) <= number_of_pages:
+        print("No longer sleeping")
+        page_url = 'https://chicago.craigslist.org/d/apartments-housing-for-rent/search/apa?s=' + str(current_page)
+        one_page = requests.get(page_url)
+        html_soup = BeautifulSoup(one_page.text, 'html.parser')
+        posts = html_soup.find_all('li', class_= 'result-row')
+
+        for one_post in posts:
+            post_one_title = one_post.find('a', class_='result-title hdrlnk')
+            post_one_id = post_one_title['id']
+            url = post_one_title['href']
+            filename = str(post_one_id) + '.txt'
+            if filepath != None:
+                filename = os.path.join(filepath, filename)
+            print(filename, url)
+            raw_html=requests.get(url)
+            with open(filename, 'w', encoding='utf-8') as file:
+                file.write(raw_html.text)
+        current_page += 120
+        print("Sleeping now")
+        time.sleep(30)
