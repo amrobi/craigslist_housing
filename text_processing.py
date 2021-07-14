06c05ff3c0f323810d329e147d39f8aa5beb8e62
asl7168 <86267412+asl7168@users.noreply.github.com>
2021-07-14 13:52:07 -0500

    Fixed path to CL_housing.csv

--- text_processing.py
+++ text_processing.py
@@ -86,7 +86,7 @@ if os.path.exists("data/csv_dumps/CL_housing.csv"):
 else: 
     open_mode = "w"
 
-with open('data/CL_housing.csv', open_mode, newline='', encoding='utf-8') as csvfile:
+with open('data/csv_dumps/CL_housing.csv', open_mode, newline='', encoding='utf-8') as csvfile:
     # these were generated from the dictionary keys at the end of all this code and just copied up here
     fieldnames = ['post_id', 'title', 'price', 'neighborhood', 'map_address', 'street_address', 'latitude', 'longitude', 'data_accuracy', 'posted', 'updated', 'available', 'housing_type', 'bedrooms', 'bathooms', 'laundry', 'parking', 'sqft', 'flooring', 'rent_period', 'app_fee', 'broker_fee', 'cats_ok', 'dogs_ok', 'no_smoking', 'furnished', 'wheelchair_access', 'AC', 'EV_charging', 'posting_body', 'images', 'url']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
